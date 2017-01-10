3c07350e8c74f02b35623d703e247492881a2d43
Xavier Gillard <xavier.gillard@uclouvain.be>
2017-01-10 17:03:25 +0100

    new attempt at the same

--- dependencies/NuSMV/reconfiguration_needed.py
+++ dependencies/NuSMV/reconfiguration_needed.py
@@ -1,5 +1,6 @@
-# pylint: disable=R0401
-# This one is reported unduly. Moreover, it isn't really part of the codebase,
+# pylint: skip-file
+# pylint: disable-all
+# R0401 is reported unduly. Moreover, it isn't really part of the codebase,
 # this script is only used during the build phase to determine whether or not
 # a re configuration of NuSMV should be issued.
 '''
